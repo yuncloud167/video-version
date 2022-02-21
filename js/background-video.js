6f4063558254444c94bc9cb9df9cb828644722ca
yunsyu <service@yunsyu.com>
2022-02-21 15:23:54 +0800

    變更首頁的title定位

--- js/background-video.js
+++
@@ -1,8 +0,0 @@
-// IE 時，改變 class name
-const video = document.getElementById('video');
-video.oncanplaythrough = () => {
-    if (navigator.userAgent.match("MSIE")) {
-        const videoEL = document.getElementById('js-background-video');
-        videoEL.setAttribute('class', 'background-video ie');
-    }
-};
